c0f5a476ea2bc48022015bdbac7f452be3e036e5
Alex Gomes <alex.gsone@gmail.com>
2025-02-01 10:01:35 -0300

    Update functions.py

--- product_handler/functions.py
+++ product_handler/functions.py
@@ -1,7 +1,14 @@
+import menu
+
 
 def get_product_by_id(int: id):
-    pass
+    for i in menu:
+        print(menu.products)
 
 
 def get_products_by_type(string: type):
     pass
+
+
+if __name__ == "__main__":
+    get_product_by_id(1)  # Exemplo de chamada de função
