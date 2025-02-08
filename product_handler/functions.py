a5406a6808882042afc0cc696a8aa1309fa8eb44
Alex Gomes <alex.gsone@gmail.com>
2025-02-08 12:37:41 -0300

    Atualização de funções

--- product_handler/functions.py
+++ product_handler/functions.py
@@ -1,9 +1,19 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import menu
 
 
-def get_product_by_id(int: id):
-    for i in menu:
-        print(menu.products)
+def get_product_by_id(id: int):
+    for product in menu.products:
+        if (product["_id"] == id):
+            print(product)
+            break
+        else:
+            print("k")
+            print(len(menu.products))
 
 
 def get_products_by_type(string: type):
@@ -11,4 +21,5 @@ def get_products_by_type(string: type):
 
 
 if __name__ == "__main__":
-    get_product_by_id(1)  # Exemplo de chamada de função
+
+    get_product_by_id(2)
