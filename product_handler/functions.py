8fc04b5de7bb528d27d9a5313b5158ac20850df6
Alex Gomes <alex.gsone@gmail.com>
2025-02-09 15:40:18 -0300

    Update functions.py

--- product_handler/functions.py
+++ product_handler/functions.py
@@ -1,25 +1,32 @@
 import sys
 import os
-
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import menu
 
 
 def get_product_by_id(id: int):
+    i = 1
     for product in menu.products:
         if (product["_id"] == id):
             print(product)
             break
-        else:
-            print("k")
-            print(len(menu.products))
+        elif (i == len(menu.products)):
+            emptyDict = {}
+            print(emptyDict)
+        i = i+1
 
 
-def get_products_by_type(string: type):
-    pass
+def get_products_by_type(type):
+
+    lista = []
+    for product in menu.products:
+        if (product["type"] == type):
+            lista.append(product)
+
+    return lista
 
 
 if __name__ == "__main__":
 
-    get_product_by_id(2)
+    print(get_products_by_type('drink'))
