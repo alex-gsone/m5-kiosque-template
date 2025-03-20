4be1bd8f316138a2b3e92bc06d88d6bb45518eaa
Alex Gomes <alex.gsone@gmail.com>
2025-03-20 18:31:32 -0300

    Tarefa 3 finalizada!

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from management.product_handler import add_product
+from management.product_handler import get_product_by_id
+from management.product_handler import get_products_by_type
+from management.product_handler import menu_report
 from management.tab_handler import calculate_tab
-
 from menu import products
-
 
 if __name__ == "__main__":
     # Seus prints de teste aqui
+    '''
     table_1 = [{"_id": 1, "amount": 5}, {"_id": 19, "amount": 5}]
     table_2 = [
         {"_id": 10, "amount": 3},
@@ -15,3 +17,9 @@ if __name__ == "__main__":
 
     print(calculate_tab(table_1))
     print(calculate_tab(table_2))
+    '''
+    #print(get_product_by_id(28))
+
+    #print(get_products_by_type(12))
+
+    #menu_report()
--- management/product_handler.py
+++ management/product_handler.py
@@ -3,23 +3,30 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import menu
-
+from menu import products
 
 def get_product_by_id(id: int):
-    for product in menu.products:
-        if (product["_id"] == id):
-            return product
-    return {}
-
+    try:
+        if not isinstance(id, int):
+            raise TypeError 
+        for product in menu.products:
+            if (product["_id"] == id):
+                return product
+        return {}
+    except TypeError:
+        return ("Product ID must be an integer")
 
 def get_products_by_type(type):
     lista = []
-    for product in menu.products:
-        if (product["type"] == type):
-            lista.append(product)
-
-    return lista
-
+    try:
+        if not isinstance(type, str):
+            raise TypeError
+        for product in menu.products:
+            if (product["type"] == type):
+                lista.append(product)
+        return lista
+    except TypeError:
+        return ("product type must be a str")
 
 def add_product(menu, **new_product):
     id = 0
@@ -30,3 +37,13 @@ def add_product(menu, **new_product):
     new_product["_id"] = 1+id
     menu.append(new_product)
     return new_product
+
+def menu_report():
+    contagemTotal = len(products)
+    somaTotal = 0
+    for product in products:
+        somaTotal += product["price"]
+
+    media = somaTotal/contagemTotal
+    print(f"\n Products Count: {contagemTotal}"
+                f" - Average Price: {media:.2f} - Most Common Type: <tipo_mais_comum>")
